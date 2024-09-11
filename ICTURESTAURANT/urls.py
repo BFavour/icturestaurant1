62f79e1ad73d4f86312ff9eac877193d9c421850
BFavour <140213441+BFavour@users.noreply.github.com>
2024-09-11 19:25:28 +0100

    Add files via upload

---
+++ ICTURESTAURANT/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for ICTURESTAURANT project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.shortcuts import render
+from django.urls import path
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', lambda request: render(request, 'index.html'), name='home'),
+    path('contact/', lambda request: render(request, 'contact_us.html'), name='contact'),
+    path('about_us/', lambda request: render(request, 'about_us.html'), name='about_us'),
+    path('', include('Restaurant.urls')),
+
+]
+if settings.DEBUG: 
+ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
